197ea5320844ce24a3a3b333cd4c7800101aa501
Iker Garcia Ramirez <iker.garcia@frenetic.ai>
2024-01-16 12:19:09 +0100

    feat(anagram): :sparkles: add a example logic

---
+++ src/anagram/flipasg/index.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from 'vitest';
+import anagram from '.';
+
+describe('flipasg > Anagram', () => {
+  test('should return the same string if input is "a"', () => {
+    const input = 'a';
+
+    expect(anagram(input)).toBe('a')
+  });
+});
---
+++ src/anagram/flipasg/index.ts
@@ -0,0 +1,3 @@
+export default function anagram(word: string){
+  return 'a';
+}
