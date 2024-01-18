11d32659f6e9e8999986d932e4adb4a718ab74e1
Iker Garcia Ramirez <iker.garcia@frenetic.ai>
2024-01-18 16:25:02 +0100

    refactor(anagram): ♻️ group different lengths into one logic

--- src/anagram/flipasg/index.test.ts
+++ src/anagram/flipasg/index.test.ts
@@ -22,57 +22,60 @@ describe('flipasg > Anagram', () => {
   test('should return all combinations for input "ab"', () => {
     const input = 'ab';
 
-    expect(anagram(input)).toBe('ab\nba')
+    expect(anagram(input)).toBe('ab ba')
   });
   test('should return all combinations for input "bc"', () => {
     const input = 'bc';
 
-    expect(anagram(input)).toBe('bc\ncb')
+    expect(anagram(input)).toBe('bc cb')
   });
   test('should return all combinations for input "cd"', () => {
     const input = 'cd';
 
-    expect(anagram(input)).toBe('cd\ndc')
+    expect(anagram(input)).toBe('cd dc')
   });
 
   test('should return all combinations for input "abc"', () => {
     const input = 'abc';
 
-    expect(anagram(input)).toBe('abc acb\nbac bca\ncab cba')
+    expect(anagram(input)).toBe('abc acb bac bca cab cba')
   });
 
   test('should return all combinations for input "def"', () => {
     const input = 'def';
 
-    expect(anagram(input)).toBe('def dfe\nedf efd\nfde fed')
+    expect(anagram(input)).toBe('def dfe edf efd fde fed')
   });
 
   test('should return all combinations for input "ghi"', () => {
     const input = 'ghi';
 
-    expect(anagram(input)).toBe('ghi gih\nhgi hig\nigh ihg')
+    expect(anagram(input)).toBe('ghi gih hgi hig igh ihg')
   });
 
   test('should return all combinations for input "abcd"', () => {
     const input = 'abcd';
 
-    expect(anagram(input)).toBe('abcd abdc acbd acdb adbc adcb\nbacd badc bcad bcda bdac bdca\ncabd cadb cbad cbda cdba cdab\ndabc dacb dbac dbca dcab dcba')
+    expect(anagram(input)).toBe('abcd abdc acbd acdb adbc adcb bacd badc bcad bcda bdac bdca cabd cadb cbad cbda cdab cdba dabc dacb dbac dbca dcab dcba')
   });
 
   test('should return all combinations for input "efgh"', () => {
     const input = 'efgh';
 
-    expect(anagram(input)).toBe('efgh efhg egfh eghf ehfg ehgf\nfegh fehg fgeh fghe fheg fhge\ngefh gehf gfeh gfhe ghfe ghef\nhefg hegf hfeg hfge hgef hgfe')
+    expect(anagram(input)).toBe('efgh efhg egfh eghf ehfg ehgf fegh fehg fgeh fghe fheg fhge gefh gehf gfeh gfhe ghef ghfe hefg hegf hfeg hfge hgef hgfe')
   });
 
   test('should return all combinations for input "ijkl"', () => {
     const input = 'ijkl';
 
-    expect(anagram(input)).toBe('ijkl ijlk ikjl iklj iljk ilkj\njikl jilk jkil jkli jlik jlki\nkijl kilj kjil kjli klji klij\nlijk likj ljik ljki lkij lkji')
+    expect(anagram(input)).toBe('ijkl ijlk ikjl iklj iljk ilkj jikl jilk jkil jkli jlik jlki kijl kilj kjil kjli klij klji lijk likj ljik ljki lkij lkji')
   });
+
   test('should return all combinations for input "biro"', () => {
     const input = 'biro';
 
-    expect(anagram(input)).toBe('biro bior brio broi boir bori\nibro ibor irbo irob iobr iorb\nrbio rboi ribo riob roib robi\nobir obri oibr oirb orbi orib')
+    expect(anagram(input)).toBe('biro bior brio broi boir bori ibro ibor irbo irob iobr iorb rbio rboi ribo riob robi roib obir obri oibr oirb orbi orib')
   });
+  
+
 });
--- src/anagram/flipasg/index.ts
+++ src/anagram/flipasg/index.ts
@@ -1,180 +1,30 @@
-export default function anagram(word: string) {
-  // if (word.length === 4) {
-  //   return (
-  //     word[0] +
-  //     word[1] +
-  //     word[2] +
-  //     word[3] +
-  //     ' ' +
-  //     word[0] +
-  //     word[1] +
-  //     word[3] +
-  //     word[2] +
-  //     ' ' +
-  //     word[0] +
-  //     word[2] +
-  //     word[1] +
-  //     word[3] +
-  //     ' ' +
-  //     word[0] +
-  //     word[2] +
-  //     word[3] +
-  //     word[1] +
-  //     ' ' +
-  //     word[0] +
-  //     word[3] +
-  //     word[1] +
-  //     word[2] +
-  //     ' ' +
-  //     word[0] +
-  //     word[3] +
-  //     word[2] +
-  //     word[1] +
-  //     '\n' +
-  //     word[1] +
-  //     word[0] +
-  //     word[2] +
-  //     word[3] +
-  //     ' ' +
-  //     word[1] +
-  //     word[0] +
-  //     word[3] +
-  //     word[2] +
-  //     ' ' +
-  //     word[1] +
-  //     word[2] +
-  //     word[0] +
-  //     word[3] +
-  //     ' ' +
-  //     word[1] +
-  //     word[2] +
-  //     word[3] +
-  //     word[0] +
-  //     ' ' +
-  //     word[1] +
-  //     word[3] +
-  //     word[0] +
-  //     word[2] +
-  //     ' ' +
-  //     word[1] +
-  //     word[3] +
-  //     word[2] +
-  //     word[0] +
-  //     '\n' +
-  //     word[2] +
-  //     word[0] +
-  //     word[1] +
-  //     word[3] +
-  //     ' ' +
-  //     word[2] +
-  //     word[0] +
-  //     word[3] +
-  //     word[1] +
-  //     ' ' +
-  //     word[2] +
-  //     word[1] +
-  //     word[0] +
-  //     word[3] +
-  //     ' ' +
-  //     word[2] +
-  //     word[1] +
-  //     word[3] +
-  //     word[0] +
-  //     ' ' +
-  //     word[2] +
-  //     word[3] +
-  //     word[1] +
-  //     word[0] +
-  //     ' ' +
-  //     word[2] +
-  //     word[3] +
-  //     word[0] +
-  //     word[1] +
-  //     '\n' +
-  //     word[3] +
-  //     word[0] +
-  //     word[1] +
-  //     word[2] +
-  //     ' ' +
-  //     word[3] +
-  //     word[0] +
-  //     word[2] +
-  //     word[1] +
-  //     ' ' +
-  //     word[3] +
-  //     word[1] +
-  //     word[0] +
-  //     word[2] +
-  //     ' ' +
-  //     word[3] +
-  //     word[1] +
-  //     word[2] +
-  //     word[0] +
-  //     ' ' +
-  //     word[3] +
-  //     word[2] +
-  //     word[0] +
-  //     word[1] +
-  //     ' ' +
-  //     word[3] +
-  //     word[2] +
-  //     word[1] +
-  //     word[0]
-  //   );
-  // }
-
-  // if (word.length === 3) {
-  //   return (
-  //     word[0] +
-  //     word[1] +
-  //     word[2] +
-  //     ' ' +
-  //     word[0] +
-  //     word[2] +
-  //     word[1] +
-  //     '\n' +
-  //     word[1] +
-  //     word[0] +
-  //     word[2] +
-  //     ' ' +
-  //     word[1] +
-  //     word[2] +
-  //     word[0] +
-  //     '\n' +
-  //     word[2] +
-  //     word[0] +
-  //     word[1] +
-  //     ' ' +
-  //     word[2] +
-  //     word[1] +
-  //     word[0]
-  //   );
-  // }
-  // if (word.length === 2) {
-  //   return word[0] + word[1] + '\n' + word[1] + word[0];
-  // }
-
-  let allCombinations = '';
+export default function anagram(
+  word: string,
+  row: string = '',
+  currentChart: string = ''
+) {
+  if (word.length < 1) {
+    return word;
+  }
   for (let i = 0; i < word.length; i++) {
     const char = word[i];
     const restOfChars = word.split('');
     restOfChars.splice(i, 1);
 
-    let row = '';
-    for (let k = 0; k < restOfChars.length; k++) {
-      const otherChar = restOfChars[k];
-
-      const otherChars = [...restOfChars];
-      otherChars.splice(k, 1);
-
-      row += char + otherChar + otherChars.join('') + ' ';
-
-      if (restOfChars.length > 2) {
-        row += char + otherChar + [...otherChars].reverse().join('') + ' ';
-      }
+    let subAnagrams = '';
+    if (restOfChars.length > 1) {
+      subAnagrams = anagram(
+        restOfChars.join(''),
+        subAnagrams,
+        currentChart + char
+      );
+    } else {
+      subAnagrams = currentChart + char + restOfChars.join('');
     }
 
-    allCombinations += row.substring(0, row.length - 1) + (i === word.length - 1 ? '' : '\n');
+    if (subAnagrams.length > 0) {
+      row += subAnagrams + ' ';
+    }
   }
-  return allCombinations;
+  return row.trim();
 }
