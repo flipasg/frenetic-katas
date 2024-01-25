67bb63e337f61fae661bb36290486feadbd45b09
Iker Garcia Ramirez <iker.garcia@frenetic.ai>
2024-01-25 17:26:11 +0100

    refactor(romanic): :recycle: group V,VI numbers

--- src/romanic-numbers/index.ts
+++ src/romanic-numbers/index.ts
@@ -1,17 +1,27 @@
 export default function arabicToRomanic(arabicNumber: number) {
-  if(arabicNumber === 6){
-    return 'VI';
+  const ARABIC_ROMANIC = {
+    1: 'I',
+    5: 'V',
+  };
+
+  if(ARABIC_ROMANIC[arabicNumber]){
+    return ARABIC_ROMANIC[arabicNumber];
   }
-  if(arabicNumber === 5){
-    return 'V';
-  }
-  if(arabicNumber === 4){
-    return 'IV';
-  }
+
   let romanicNumber = '';
-  for (let index = 0; index < arabicNumber; index++) {
-    romanicNumber += 'I';
-    
+  for (let index = arabicNumber; index > 0; ) {
+    const romanicLetter = ARABIC_ROMANIC[index];
+    if(romanicLetter){
+      romanicNumber = ARABIC_ROMANIC[index] + romanicNumber;
+      index-=index;
+    } else {
+      romanicNumber += ARABIC_ROMANIC[1];
+      index--;
+    }
+  }
+
+  if(romanicNumber.length === 4){
+    return 'IV';
   }
   return romanicNumber;
 }
